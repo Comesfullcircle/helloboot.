3629b193ed085db0b7fc419f1ce54d36250d0558
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-01-30 18:58:21 +0900

    Spring JDBC 자동 구성 개발 -리포지토리를 사용하는 HelloService

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,14 +1,32 @@
 package tobyspring.helloboot;
 
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
-public class HellobootApplication {
+public class HellobootApplication implements InitializingBean {
+	private final JdbcTemplate jdbcTemplate;
+
+	public HellobootApplication(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@PostConstruct
+	void init() {
+		jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+	}
 
 	public static void main(String[] args){
 		SpringApplication.run(HellobootApplication.class, args);
 	}
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+
+	}
 }
